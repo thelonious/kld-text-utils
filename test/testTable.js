f066a4f21b08f53540fdabd5fbd6a88c4d56b089
Kevin Lindsey <kevin@kevlindev.com>
2016-10-21 08:59:40 -0700

    Allow the header divider to be changed

--- test/testTable.js
+++ test/testTable.js
@@ -42,4 +42,25 @@ describe('Table', function() {
 		});
 	});
 
+	describe('toString with custom divider', function() {
+		var expected = [
+			"First Middle  Last     Age",
+			"----- ------- -------- ---",
+			"John  William Dough     48",
+			"Susan Anita   Bandita   56",
+			"Joe   Ernest  Cummings   8"
+		].join("\n");
+
+		var table = new Table(["First", "Middle", "Last", "Age"]);
+
+		table.divider = "-";
+		table.addRow(["John", "William", "Dough", 48]);
+		table.addRow(["Susan", "Anita", "Bandita", 56]);
+		table.addRow(["Joe", "Ernest", "Cummings", 8]);
+
+		it("should look like", function() {
+			assert.equal(expected, table.toString());
+		});
+	});
+
 });
